9ee06081b2ad8ccd27ab1fd69e6e080fdc2e3b7c
SPA Preet <preetshivam111@gmail.com>
2021-05-01 16:43:38 +0530

    Added Class 2 and Updated the CsProj.

---
+++ MyFirstProject/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    class Class2
+    {
+        //Added one more class that is needed in the Command.
+    }
+}
