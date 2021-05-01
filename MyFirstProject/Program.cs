0853e7cb9f5dc66de4045eb2b51c330434395525
SPA Preet <preetshivam111@gmail.com>
2021-05-01 19:09:12 +0530

    Checking in changes related to program.cs.

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,8 +10,8 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Master updated First");
-            Console.WriteLine("I have updated the features and my feature works Fine");
+            Console.WriteLine("Master updated first");
+            Console.WriteLine("I have updated the features and my feature works fine");
         }
     }
 }
